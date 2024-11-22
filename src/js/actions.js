164b2de603f7a88667fde649c4082f90322d7722
Bruno Massuete <malvesbruno0@gmail.com>
2024-11-22 17:29:28 -0300

    first commit

---
+++ src/js/actions.js
@@ -0,0 +1,11 @@
+function openGithub(){
+    window.location.href = 'https://github.com/malvesbruno';
+}
+
+function openLinkedin(){
+    window.location.href = 'https://www.linkedin.com/in/bruno-massuete-7a91b3234/';
+}
+
+function openPortfolio(){
+    window.location.href = 'https://malvesbruno.github.io/NewPortifolio/';
+}
